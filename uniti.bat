ff4bbd81d40e5f737498fe75553a48f41b957281
PJTraut <pieter.traut@biblica.com>
2026-09-05 06:17:48 +0200

    fix: retain batch launcher handoff

--- uniti.bat
+++ uniti.bat
@@ -40,33 +40,33 @@ if %ERRORLEVEL% EQU 0 goto run_python312
 exit /b 10
 
 :run_override
-"%UNITI_PYTHON%" "%UNITI_BOOTSTRAP%"
+call "%UNITI_PYTHON%" "%UNITI_BOOTSTRAP%"
 exit /b %ERRORLEVEL%
 
 :run_py
-py -3 "%UNITI_BOOTSTRAP%"
+call py -3 "%UNITI_BOOTSTRAP%"
 exit /b %ERRORLEVEL%
 
 :run_python
-python "%UNITI_BOOTSTRAP%"
+call python "%UNITI_BOOTSTRAP%"
 exit /b %ERRORLEVEL%
 
 :run_python3
-python3 "%UNITI_BOOTSTRAP%"
+call python3 "%UNITI_BOOTSTRAP%"
 exit /b %ERRORLEVEL%
 
 :run_python315
-python3.15 "%UNITI_BOOTSTRAP%"
+call python3.15 "%UNITI_BOOTSTRAP%"
 exit /b %ERRORLEVEL%
 
 :run_python314
-python3.14 "%UNITI_BOOTSTRAP%"
+call python3.14 "%UNITI_BOOTSTRAP%"
 exit /b %ERRORLEVEL%
 
 :run_python313
-python3.13 "%UNITI_BOOTSTRAP%"
+call python3.13 "%UNITI_BOOTSTRAP%"
 exit /b %ERRORLEVEL%
 
 :run_python312
-python3.12 "%UNITI_BOOTSTRAP%"
+call python3.12 "%UNITI_BOOTSTRAP%"
 exit /b %ERRORLEVEL%
